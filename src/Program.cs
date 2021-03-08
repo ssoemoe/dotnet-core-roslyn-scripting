db7dbedb8b50e9065a707f33c997be2828316e3c
Shane Moe <shanekhantsoemoe98@gmail.com>
2021-03-08 02:57:38 -0500

    feat: Roslyn scripting :bookmark:

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Threading.Tasks;
 
 namespace Scripting
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = string.Empty;
+            var code = string.Empty;
+            while (!input.Trim().Equals("run"))
+            {
+                code += input;
+                Console.Write("> ");
+                input = Console.ReadLine();
+            }
+            var scriptOptions = ScriptOptions.Default;
+            scriptOptions = scriptOptions.AddReferences("System");
+            code = "using System;" + code;
+            var result = await CSharpScript.EvaluateAsync(code, scriptOptions);
+
+            /*
+            Compile once. Run many times.
+            var script = CSharpScript.Create(code);
+            for(var i =0; i < 100;i++) { var result = await script.RunAsycn(); }
+            */
+
+            Console.ReadLine();
         }
     }
 }
